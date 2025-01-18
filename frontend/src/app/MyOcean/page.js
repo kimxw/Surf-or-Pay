065f976d7b5d5e5d391abe4667273db62190b3d3
kimxw <kimaya.wanjari@nuscomputing.com>
2025-01-18 14:34:53 +0800

    Routing and landing

--- frontend/src/app/MyOcean/page.js
+++ frontend/src/app/MyOcean/page.js
@@ -1,3 +1,13 @@
 export default function MyOcean() {
-    return <p>My Ocean Page</p>;
+    return (
+    <>
+      <div
+        className='flex flex-col justify-center items-center h-screen'
+        style={{ backgroundImage: 'url(/oceanbg.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+      >
+        <p>My Ocean Page</p>
+      </div>
+    </>
+    );
+    
   }
