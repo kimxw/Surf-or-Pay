17bf4c8a9782df151884c83bf3fabc95d984f31d
kimxw <kimaya.wanjari@nuscomputing.com>
2025-01-18 14:59:55 +0800

    sidebar component

--- frontend/src/app/MyOcean/page.js
+++ frontend/src/app/MyOcean/page.js
@@ -1,13 +1,22 @@
+'use client'
+
+import React, { useState } from "react";
+import { Sidebar } from "./components/sidebar";
+
 export default function MyOcean() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
     return (
-    <>
-      <div
-        className='flex flex-col justify-center items-center h-screen'
-        style={{ backgroundImage: 'url(/oceanbg.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}
-      >
-        <p>My Ocean Page</p>
+      <div>
+        <Sidebar open={isSidebarOpen} setOpen={setIsSidebarOpen} animate={true}>
+          {/* Sidebar content goes here */}
+        </Sidebar>
+
+        {/* Your page content */}
+        <div className="content">
+          <h1>Your main content here</h1>
+        </div>
       </div>
-    </>
     );
     
   }
